e2c133335e36e6a0dd975d55a5cbe622252a58b4
OTDF Studios Official <168537533+OTDFStudiosOFFICIAL@users.noreply.github.com>
2024-11-08 22:18:07 +0000

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// JavaScript for the "Submit Element Request" button
+document.getElementById('requestButton').addEventListener('click', function() {
+    alert('Thank you for your element request! It will be reviewed by OTDF Studios.');
+});
